55a61b10504bc4e0d2d3b6594bace7028ce8e98d
wjw19941112 <wujiwei@pku.edu.cn>
2017-11-08 13:15:56 +0800

    登陆界面

---
+++ app/src/main/java/cn/edu/pku/wu/choosedormitory/LoginActivity.java
@@ -0,0 +1,18 @@
+package cn.edu.pku.wu.choosedormitory;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by wu on 2017/11/8.
+ */
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
